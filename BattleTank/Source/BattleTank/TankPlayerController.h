e1e112dbe2def7599086ab910b67925f6f13eca9
JoshuaHarward <jlharward@charter.net>
2019-02-25 18:54:54 -0600

    Lecture 136 Commit

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -12,16 +12,24 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+
 public:
-	ATank* GetControlledTank() const;
+	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
-	
-	virtual void BeginPlay() override;
+
+private:
+	ATank* GetControlledTank() const;
 
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
 
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
 };
