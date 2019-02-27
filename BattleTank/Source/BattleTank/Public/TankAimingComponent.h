e199b272c3e424105c80b0e6b5a679d2a1dc27d2
JoshuaHarward <jlharward@charter.net>
2019-02-27 16:26:00 -0600

    Part of Lecture 141 commit

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -16,14 +16,18 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+
 	void AimAt(FVector OutHitLocation);
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
+private:	
+	UStaticMeshComponent* Barrel = nullptr;
 };
