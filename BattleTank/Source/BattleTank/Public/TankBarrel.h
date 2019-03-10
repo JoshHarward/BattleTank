e40d57e610342fefd01aabe28e3a6e269cfc1e60
JoshuaHarward <jlharward@charter.net>
2019-03-09 22:47:39 -0600

    Lecture 151 Commit

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -20,7 +20,7 @@ public:
 		
 private:
 	UPROPERTY(EditAnywhere, Category = Setup) // sets the category in blueprint to be listed as 'Setup'
-	float MaxDegreesPerSecond = 20; // sensible default
+	float MaxDegreesPerSecond = 5; // sensible default
 
 	UPROPERTY(EditAnywhere, Category = Setup)
 	float MaxElevationDegrees = 40;
