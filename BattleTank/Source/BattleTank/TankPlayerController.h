9954b96d7d24e3bc8bf51943e87ed03e768c00c1
JoshuaHarward <jlharward@charter.net>
2019-03-09 19:40:54 -0600

    Forward declarations

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -2,10 +2,11 @@
 
 #pragma once
 
-#include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"  // Put new above this
+
+class ATank;
 
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
