ca7923e54dcb1129ef22924ad4934318181adc4c
JoshuaHarward <jlharward@charter.net>
2019-03-19 22:00:08 -0500

    Lecture 172 Commit

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -21,7 +21,7 @@ public:
 
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 40000000; // Assume 40 ton tank, and 1g acceleration
+	float TrackMaxDrivingForce = 20000000; // Assume 40 ton tank, and 1g acceleration
 
 private:
 
