b6eae5e925801a5f5dbf812e7e9e5d5a95335c3e
JoshuaHarward <jlharward@charter.net>
2019-03-23 17:04:17 -0500

    Lecture 181 Commit

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -7,6 +7,15 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent)
+	{
+		FoundAimingComponent(AimingComponent);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller can't find aiming component at Begin Play"))
+	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
